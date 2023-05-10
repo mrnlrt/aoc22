8401507ebe99d9a3198c39914b996db2e26769cc
Marine Loriot <mloriot@octo.com>
2023-05-10 09:24:27 +0200

    day template

---
+++ src/main/java/main/day100/Day100.java
@@ -0,0 +1,18 @@
+package main.day100;
+
+import java.util.List;
+
+public class Day100 {
+    public int part1(List<String> input) {
+        return 0;
+    }
+
+
+    public int part2(List<String> input) {
+       return 0;
+    }
+
+
+}
+
+
